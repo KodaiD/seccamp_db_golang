ee4f49837fed359bb18997f807d6362b199cfab0
KodaiD <bb_dkoudai@yahoo.co.jp>
2020-10-27 20:57:04 +0900

    rough model

--- main.go
+++ main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// supported operation
+const (
+	READ = 1 + iota
+	INSERT
+	UPDATE
+	DELETE
+	EXIT
+)
 
 func main() {
-	fmt.Println("initial commit")
+	fmt.Println("starting db...")
+
+	index := make(map[string]string)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("seccamp-db >> ")
+		if scanner.Scan() {
+			input := strings.Fields(scanner.Text())
+			cmd := input[0]
+			switch cmd {
+			case "read":
+				if len(input) != 2 {
+					fmt.Println("wrong format -> read <key>")
+				}
+				key := input[1]
+				value, exist := index[key]
+				if !exist {
+					fmt.Println("key not exists")
+					continue
+				}
+				fmt.Println(value)
+			case "insert":
+				if len(input) != 3 {
+					fmt.Println("wrong format -> insert <key> <value>")
+				}
+				key, value := input[1], input[2]
+				_, exist := index[key]
+				if exist {
+					fmt.Println("key already exists")
+					continue
+				}
+				index[key] = value
+			case "update":
+				if len(input) != 3 {
+					fmt.Println("wrong format -> update <key> <value>")
+				}
+				key, value := input[1], input[2]
+				_, exist := index[key]
+				if !exist {
+					fmt.Println("key not exists")
+					continue
+				}
+				index[key] = value
+			case "delete":
+				if len(input) != 2 {
+					fmt.Println("wrong format -> delete <key>")
+				}
+				key := input[1]
+				_, exist := index[key]
+				if !exist {
+					fmt.Println("key not exists")
+					continue
+				}
+				delete(index, key)
+			case "exit":
+				os.Exit(0)
+			default:
+				fmt.Println("command not supported")
+			}
+
+		}
+	}
 }
